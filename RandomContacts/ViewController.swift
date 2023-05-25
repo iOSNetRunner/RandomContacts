aa625d5addf61a1e78cb775d539fbccfef988192
NetRunner <galatskiy@gmail.com>
2023-05-25 22:21:07 +0300

    Initial Commit

---
+++ RandomContacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomContacts
+//
+//  Created by Dmitrii Galatskii on 25.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
