27a96d41f4ae4dd2c29eaa155309d7d50b4dbd02
NetRunner <galatskiy@gmail.com>
2023-05-26 22:13:16 +0300

    Person model + algorithm ready.

--- RandomContacts/Models/Person.swift
+++ RandomContacts/Models/Person.swift
@@ -5,4 +5,40 @@
 //  Created by Dmitrii Galatskii on 25.05.2023.
 //
 
-
+struct Person {
+    let name: String!
+    let lastname: String!
+    let phoneNumber: String!
+    let email: String!
+    
+    static func getContactList() -> [Person] {
+        var names = DataStore.shared.names
+        var lastnames = DataStore.shared.lastnames
+        var phoneNumbers = DataStore.shared.phoneNumbers
+        var emails = DataStore.shared.emails
+        
+        var contacts: [Person] = []
+        
+        for _ in 1...names.count {
+            let randomName = names.indices.randomElement() ?? 0
+            let uniqueName = names.remove(at: randomName)
+            
+            let randomLastname = lastnames.indices.randomElement() ?? 0
+            let uniqueLastname = lastnames.remove(at: randomLastname)
+            
+            let randomPhoneNumber = phoneNumbers.indices.randomElement() ?? 0
+            let uniquePhoneNumber = phoneNumbers.remove(at: randomPhoneNumber)
+            
+            let randomEmail = emails.indices.randomElement() ?? 0
+            let uniqueEmail = emails.remove(at: randomEmail)
+            
+            contacts.append(
+                Person(name: uniqueName,
+                       lastname: uniqueLastname,
+                       phoneNumber: uniquePhoneNumber,
+                       email: uniqueEmail)
+            )
+        }
+        return contacts
+    }
+}
